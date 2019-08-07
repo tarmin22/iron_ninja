f4d6ee9c86bee7f2762a5031a7f585b710d251d4
Armin Esmaeili-Tehrani <tarmin@cox.net>
2019-08-07 13:59:39 -0700

    added fixed to the main program

--- Program.cs
+++ Program.cs
@@ -17,10 +17,31 @@ namespace iron_ninja
             Aiden.Consume(mixed.Serve());
             Aiden.Consume(mixed.Serve());
             Aiden.Consume(mixed.Serve());
+
+
             Aaron.Consume(mixed.Serve());
             Aaron.Consume(mixed.Serve());
-            Aaron.Consume(mixed.Serve());
-            Aaron.Consume(mixed.Serve());
+
+            int count1 = Aaron.ConsumptionHistory.Count;
+            int count2 = Aiden.ConsumptionHistory.Count;
+            if (count1 > count2)
+            {
+                System.Console.WriteLine(Aaron + "Consumed more than " + Aiden + "!");
+                System.Console.WriteLine("Aaron consumed" + count1);
+            }
+            else if (count1 == count2)
+            {
+                System.Console.WriteLine("Both " + Aaron + " and " + Aiden + " consumed the same amount!");
+                System.Console.WriteLine(Aiden + " consumed " + count2);
+                System.Console.WriteLine("Aaron consumed" + count1);
+
+            }
+            else if (count1 < count2)
+            {
+                System.Console.WriteLine(Aiden + "Consumed more than " + Aaron + "!");
+                System.Console.WriteLine(Aiden + " consumed " + count2);
+            }
+
 
         }
     }
