47fad7c4da490b12ecf882fdb1626fd3b530f25f
Armin Esmaeili-Tehrani <tarmin@cox.net>
2019-08-06 22:47:52 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace iron_ninja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
